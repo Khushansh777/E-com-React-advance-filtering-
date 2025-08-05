9c51049f327af0bb2e7a5bb60b7c5397d66ec12c
Khushansh <khushanshbaliyan@gmail.com>
2025-08-06 00:26:13 +0530

    Refactor App component to include state management for search and category selection; replace static data with dynamic product filtering from new data source.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
+import { useState } from "react";
 import Navbar from "./Navigation/Navbar";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
-import Category from "./Sidebar/Category/Category";
-import Colors from "./Sidebar/Color/Colors";
-import Price from "./Sidebar/Price/Price";
 import Sidebar from "./Sidebar/Sidebar";
-
+import products from "./db/data.jsx";
 function App() {
+  const [query, setQuery] = useState("");
+  const [selCategory, setSelCategory] = useState(null);
+  // handle query
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+  };
+  // handle Category
+  const handleCategory = (e) => {
+    setSelCategory(e.target.value);
+  };
+
+  // filtered items
+  const filteredItems = products.filter((products) => {
+    return products.title
+      .toLocaleLowerCase()
+      .includes(query.toLocaleLowerCase());
+  });
+
+  
+
   return (
     <div>
-    <Sidebar />
+      <Sidebar />
       <Navbar />
-       
       <Recommended />
-      
       <Products />
-     
-     
     </div>
   );
 }
